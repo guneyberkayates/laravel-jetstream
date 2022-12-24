e111e7f37d9da53967af3699ceb50aec50cb36b4
Guney Berkay <guneyberkayates@outlook.com>
2022-12-25 01:16:59 +0300

    quiz app

---
+++ quiz/quiz/resources/views/admin/quiz/list.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">Testler</x-slot>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">
+                <a href="#" class="btn btn-sm btn-primary">Test Oluştur</a>
+            </h5>
+            Lorem ipsum dolor sit amet ..........................
+        </div>
+    </div>
+</x-app-layout>
---
+++ quiz/quiz/resources/views/admin\quiz/list.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-slot name="header">Testler</x-slot>
+
+    <div class="alert alert-danger">Bootstrap alert mesajı</div>
+
+</x-app-layout>
